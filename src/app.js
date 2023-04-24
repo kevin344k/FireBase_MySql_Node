7ca5920ba485b36df9a78b18b1c3da7354f42784
kevin344k <kevin344k@gmail.com>
2023-04-23 21:58:23 -0500

    proyecto de faztweb

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express =require('express')
+
+const morgan=require('morgan')
+const exphbs=require('express-handlebars')
+
+const app=express()
+
+//settings
+
+app.set('port',process.env.PORT || 7000)
+
+
+//middlewares
+
+
+
+//routes
+
+
+
+
+module.exports=app;
